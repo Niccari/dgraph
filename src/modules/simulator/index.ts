6b5cc20c7c822a5ce7241a6b67dc0db12eee777f
Niccari <latieos93@gmail.com>
2022-07-31 21:48:43 +0900

    fix: tuning performance for math evaluation

--- src/modules/simulator/index.ts
+++ src/modules/simulator/index.ts
@@ -1,23 +1,26 @@
-import { evaluate } from "mathjs";
+import { parser } from "mathjs";
 import { ISimulator } from "./interface";
 import { Value } from "../../models";
 
 class Simulator implements ISimulator {
   // eslint-disable-next-line class-methods-use-this
-  public create = async (equation: string, x: number[]): Promise<Value[]> =>
-    Promise.all(
+  public create = async (equation: string, x: number[]): Promise<Value[]> => {
+    const fx = parser();
+    fx.evaluate(`f(x) = ${equation}`)
+    return Promise.all(
       x.map(async (xi) => {
         try {
-          const result = evaluate(equation, { x: xi });
+          const result = fx.evaluate(`f(${xi})`) as number | undefined;
           if (typeof result === "number") {
             return { x: xi, y: result };
           }
           return { x: xi, y: NaN };
-        } catch {
-          return Promise.reject(new Error("Cannot eval"));
+        } catch (e) {
+          return Promise.reject(new Error(`Cannot eval: ${e}`));
         }
       })
     );
+  }
 }
 
 export default Simulator;

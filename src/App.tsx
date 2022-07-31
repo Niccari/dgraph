c1f97a6bb070389548ae069481145dd791565a42
Niccari <latieos93@gmail.com>
2022-07-31 22:02:17 +0900

    fix: remove redundant simulation when mounting app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import { useEffect } from "react";
 import "./App.css";
 import Canvas from "./components/Canvas";
 import QueryUpdater from "./components/QueryUpdater";
 import SettingForm from "./components/SettingForm";
-import { useAppDispatch, useAppSelector } from "./redux/hooks";
-import { simulateAsync } from "./redux/slice/simulationSlice";
 
-const App = (): JSX.Element => {
-  const dispatch = useAppDispatch();
-  const setting = useAppSelector((state) => state.simulation.setting);
-
-  useEffect(() => {
-    dispatch(simulateAsync({ setting }));
-  }, []);
-
-  return (
+const App = (): JSX.Element => (
     <>
       <SettingForm />
       <Canvas />
       <QueryUpdater />
     </>
   );
-};
 
 export default App;

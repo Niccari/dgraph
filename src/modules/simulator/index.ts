82591bfd98ab33e7acecea957d1b6d92072a6051
Niccari <latieos93@gmail.com>
2025-06-01 16:58:30 +0900

    fix: replace math evaluation libs

--- src/modules/simulator/index.ts
+++ src/modules/simulator/index.ts
@@ -1,16 +1,18 @@
-import { parser } from "mathjs";
+import Mexp from "math-expression-evaluator";
 import { Value } from "../../models";
 import { ISimulator } from "./interface";
 
 class Simulator implements ISimulator {
   // eslint-disable-next-line @typescript-eslint/class-methods-use-this
   public create = async (equation: string, x: number[]): Promise<Value[]> => {
-    const fx = parser();
-    fx.evaluate(`f(x) = ${equation}`);
+    const mexp = new Mexp();
     return Promise.all(
       x.map(async (xi) => {
         try {
-          const result = fx.evaluate(`f(${xi})`) as number | undefined;
+          // added parenthesis to miss replacing
+          // ex) expected: 5.01x -> 5.01 * 2.3, not to be: 5.1x -> 5.12.3 when x = 2.3)
+          const equationWithValue = equation.replace(/x/g, `(${(xi * 180) / Math.PI})`);
+          const result = mexp.eval(equationWithValue);
           if (typeof result === "number") {
             return { x: xi, y: result };
           }
